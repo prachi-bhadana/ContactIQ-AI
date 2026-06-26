a5973dcdd3c103b6c1e2248fe21b224bea250bdc
prachi-bhadana <bhadanaprachi5@gmail.com>
2026-06-26 23:01:37 +0530

    phase 2 : Integrated Gemini API setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from google import genai
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
+load_dotenv()
+api_key= os.getenv("GEMINI_API_KEY")
+
+client= genai.Client(api_key=api_key)
+
 app = FastAPI()
 
 class ContactInput(BaseModel):
@@ -13,5 +21,20 @@ def home ():
 
 @app.post("/extract")
 def extract_contact(data: ContactInput):
-    return {"received_text": data.text,
-            "status":"successfully received!"}
+       result =process_text(data.text)
+       return {
+            "received_text":result,
+            "status" : "successfully received!"
+       }
+def process_text(text):
+    response = client.models.generate_content(
+     model="gemini-2.5-flash",
+     contents=text
+    )
+    return response.text
+
+
+ 
+
+
+ 
